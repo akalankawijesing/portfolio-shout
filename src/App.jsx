d65e2a26bfa0dc012f8edef5b6eb1a9aacb577e5
Nirmal W <akalankawijesing@gmail.com>
2024-10-17 19:49:26 +0530

    fixed bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import Header from './components/Header'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-  <Header/>
-
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
